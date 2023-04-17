0cd70e3a3a694061de97ee11b5ec39de70a1a7f1
Sanda Mandresy <98638893+Sanmandresy@users.noreply.github.com>
2023-04-17 15:47:51 +0300

    initial commit

---
+++ src/main/java/mg/hei/tdprog3/TdProg3Application.java
@@ -0,0 +1,13 @@
+package mg.hei.tdprog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdProg3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdProg3Application.class, args);
+	}
+
+}
---
+++ src/test/java/mg/hei/tdprog3/TdProg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.hei.tdprog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdProg3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
